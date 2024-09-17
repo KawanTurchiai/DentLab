a809a3d7b9db719401d0d1dad3fe8799cd4404b2
KawanTurchiai <kawanbarbosa150@gmail.com>
2024-09-17 11:42:30 -0300

    Atualização Projeto DentLab

--- js/register.js
+++ js/register.js
@@ -1,7 +1,9 @@
+// register.js
+
 // Função para registrar usuário
 function registerUser(email, password) {
-    // Pegar os usuários armazenados no localStorage
-    let users = JSON.parse(localStorage.getItem('users')) || [];
+    //Pegar os usuários armazenados no localStorafe
+    let users =JSON.parse(localStorage.getItem('users')) || [];
 
     const userExists = users.some(user => user.email === email);
 
@@ -9,9 +11,9 @@ function registerUser(email, password) {
         alert('Usuário já registrado');
     } else {
         // Adicionar o novo usuário
-        users.push({ email, password });
+        users.push({ email, password});
         
-        // Salvar de volta no localStorage
+        // Salvar de volta no LocalStorage
         localStorage.setItem('users', JSON.stringify(users));
 
         alert('Cadastro realizado com sucesso');
@@ -23,13 +25,17 @@ document.getElementById('registerButton').addEventListener('click', (event) => {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+    const confirmpassword = document.getElementById('confirmpassword').value;
 
     // Verificar se as senhas são iguais
-    if (password !== confirmPassword) {
+
+    if (password !== confirmpassword) {
         alert('Senhas não são iguais!');
     } else {
         // Chamar a função de registro
         registerUser(email, password);
     }
+    
 });
+
+        
